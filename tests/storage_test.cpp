001e8ae34b92fff174b9d3c0a6b427778d163406
Chenming Shi <clevercshi@gmail.com>
2021-05-02 16:33:56 +0800

    added tests on size of insertion

--- tests/storage_test.cpp
+++ tests/storage_test.cpp
@@ -25,9 +25,49 @@
 //     db.close();
 // }
 
-TEST(StorageTest, Tiering)
+// TEST(StorageTest, Tiering)
+// {
+//     std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_tiering_db_ratio6"; 
+//     templatedb::DB db;
+//     db.open(fname, templatedb::TIERING);
+//     std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_5000000_4.data";
+//     db.load_data_file(data);
+//     db.close();
+// }
+
+// TEST(StorageTest, Leveling)
+// {
+//     std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_leveling_db_ratio6"; 
+//     templatedb::DB db;
+//     db.open(fname, templatedb::LEVELING);
+//     std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_5000000_4.data";
+//     db.load_data_file(data);
+//     db.close();
+// }
+
+TEST(StorageTest, Tiering100)
 {
-    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_tiering_db_ratio6"; 
+    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_tiering_db_100"; 
+    templatedb::DB db;
+    db.open(fname, templatedb::TIERING);
+    std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_1000000_4.data";
+    db.load_data_file(data);
+    db.close();
+}
+
+TEST(StorageTest, Leveling100)
+{
+    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_leveling_db_100"; 
+    templatedb::DB db;
+    db.open(fname, templatedb::LEVELING);
+    std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_1000000_4.data";
+    db.load_data_file(data);
+    db.close();
+}
+
+TEST(StorageTest, Tiering500)
+{
+    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_tiering_db_500"; 
     templatedb::DB db;
     db.open(fname, templatedb::TIERING);
     std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_5000000_4.data";
@@ -35,12 +75,52 @@ TEST(StorageTest, Tiering)
     db.close();
 }
 
-TEST(StorageTest, Leveling)
+TEST(StorageTest, Leveling500)
 {
-    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_leveling_db_ratio6"; 
+    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_leveling_db_500"; 
     templatedb::DB db;
     db.open(fname, templatedb::LEVELING);
     std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_5000000_4.data";
+    db.load_data_file(data);
+    db.close();
+}
+
+TEST(StorageTest, Tiering1000)
+{
+    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_tiering_db_1000"; 
+    templatedb::DB db;
+    db.open(fname, templatedb::TIERING);
+    std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_5000000_4.data";
+    db.load_data_file(data);
+    db.close();
+}
+
+TEST(StorageTest, Leveling1000)
+{
+    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_leveling_db_1000"; 
+    templatedb::DB db;
+    db.open(fname, templatedb::LEVELING);
+    std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_10000000_4.data";
+    db.load_data_file(data);
+    db.close();
+}
+
+TEST(StorageTest, Tiering2000)
+{
+    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_tiering_db_2000"; 
+    templatedb::DB db;
+    db.open(fname, templatedb::TIERING);
+    std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_20000000_4.data";
+    db.load_data_file(data);
+    db.close();
+}
+
+TEST(StorageTest, Leveling2000)
+{
+    std::string fname = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/test_leveling_db_2000"; 
+    templatedb::DB db;
+    db.open(fname, templatedb::LEVELING);
+    std::string data = "/mnt/d/files/BU/CS 561/projects/LSM/cs561_templatedb/data/test_20000000_4.data";
     db.load_data_file(data);
     db.close();
 }
